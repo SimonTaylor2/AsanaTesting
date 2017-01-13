b7510177707e43f7d3c66ac04ba1e8f771496193
SimonTaylor2 <Simon.Taylor@freewayinsurance.co.uk>
2017-01-13 16:53:43 +0000

    BRD - Added new textboxes for capturing claims

---
+++ AsanaApp/Controllers/DepartmentListModelsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AsanaApp.Models;
+using System.Web.Http.Filters;
+
+namespace AsanaApp.Controllers
+{
+    public class DepartmentListModelsController : ApiController
+    {
+        private COMMON_TEST db = new COMMON_TEST();
+
+        // GET: api/DepartmentListModels
+        public IQueryable<DepartmentListModels> GetDepartmentListModels()
+        {
+            return db.DepartmentListModels;
+        }
+
+
+        // GET: api/DepartmentListModels/5
+        [ResponseType(typeof(DepartmentListModels))]
+        public IHttpActionResult GetDepartmentListModels(int id)
+        {
+            DepartmentListModels departmentListModels = db.DepartmentListModels.Find(id);
+            if (departmentListModels == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(departmentListModels);
+        }
+
+        // PUT: api/DepartmentListModels/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDepartmentListModels(int id, DepartmentListModels departmentListModels)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != departmentListModels.DEPARTMENT_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(departmentListModels).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentListModelsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/DepartmentListModels
+        [ResponseType(typeof(DepartmentListModels))]
+        public IHttpActionResult PostDepartmentListModels(DepartmentListModels departmentListModels)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.DepartmentListModels.Add(departmentListModels);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = departmentListModels.DEPARTMENT_ID }, departmentListModels);
+        }
+
+        // DELETE: api/DepartmentListModels/5
+        [ResponseType(typeof(DepartmentListModels))]
+        public IHttpActionResult DeleteDepartmentListModels(int id)
+        {
+            DepartmentListModels departmentListModels = db.DepartmentListModels.Find(id);
+            if (departmentListModels == null)
+            {
+                return NotFound();
+            }
+
+            db.DepartmentListModels.Remove(departmentListModels);
+            db.SaveChanges();
+
+            return Ok(departmentListModels);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DepartmentListModelsExists(int id)
+        {
+            return db.DepartmentListModels.Count(e => e.DEPARTMENT_ID == id) > 0;
+        }
+    }
+}
---
+++ AsanaApp/Models/DepartmentListModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AsanaApp.Models
+{
+    [Table("dbo.LIST_DEPARTMENTS")]
+    public class DepartmentListModels
+    {
+        [Key]
+        public int DEPARTMENT_ID { get; set; }
+        public string DEPARTMENT { get; set; }
+    }
+}
